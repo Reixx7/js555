53f5ab421ee123e6532d40af5d9ae8edfb63e989
Reixx7 <reixx23and7@gmail.com>
2025-03-27 10:12:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let name = prompt("Ismingizni kiriting");
+let id = prompt("Birorta harf kiriting");
+let password = prompt("Space password kiriting");
+
+const userData = { name, id, password };
+console.log(userData);
